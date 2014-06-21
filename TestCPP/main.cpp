98e846d3db302fc71de82834c1c4e0459426fd53
sun <wzhsunn@gmail.com>
2014-06-21 13:34:36 +0800

    Initial Commit

---
+++ TestCPP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestCPP
+//
+//  Created by zcomer on 14-6-21.
+//  Copyright (c) 2014年 zcomer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
